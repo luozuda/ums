1030b932adec62fd1c90368a17d6328c3aa37d4a
zhangsan <luozuda@163.com>
2019-06-12 17:39:08 +0800

    update validate

--- src/components/AddCustomer.vue
+++ src/components/AddCustomer.vue
@@ -4,7 +4,13 @@
       <a-input
         v-decorator="[
           'name',
-          { rules: [{ required: true, message: '请输入用户名！' }] }
+          { 
+            rules: [{ 
+              required: true, message: '用户名不能为空！' 
+            },{
+              max:11,message:'用户名不能超过11个字符！'
+            }] 
+          }
         ]"
         placeholder="name"
       ></a-input>
@@ -13,7 +19,13 @@
       <a-input
         v-decorator="[
           'phone',
-          { rules: [{ required: true, message: '请输入手机！' }] }
+          { 
+            rules: [{ 
+              required: true, message: '手机不能为空！' 
+            },{
+              pattern:/^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,message:'请输入11位的有效手机号！'
+            }] 
+          }
         ]"
         placeholder="phone"
       ></a-input>
